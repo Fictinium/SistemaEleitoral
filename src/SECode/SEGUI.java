2072e81f4103fe26073b5953812144c9b9f44442
Fictinium <aluno23053@ipt.pt>
2022-11-23 15:39:17 +0000

    mudança de pacote

--- src/SECode/SEGUI.java
+++ src/SECode/SEGUI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package SEGUI;
+package SECode;
 
 import SECode.Eleicao;
 import SECode.Eleitor;
